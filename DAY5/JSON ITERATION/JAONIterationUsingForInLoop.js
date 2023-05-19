1b2ef3205bd32457374b22ed2e5e41220c4d9f14
parthibanb224 <parthibanb224@gmail.com>
2023-05-19 12:38:39 +0530

    DAY5 TASK

---
+++ DAY5/JSON ITERATION/JAONIterationUsingForInLoop.js
@@ -0,0 +1,105 @@
+let arr = [
+    {
+        "id": "1",
+        "name": "Parthiban",
+        "email": "parthiban@yahoo.com"
+    },
+    {
+        "id": "2",
+        "name": "Muthuprabhu",
+        "email": "muthu234@yahoo.com"
+    },
+    {
+        "id": "3",
+        "name": "Karthi",
+        "email": "karthi99@yahoo.com"
+    },
+    {
+        "id": "4",
+        "name": "Ragunath",
+        "email": "randi321@yahoo.com"
+    },
+    {
+        "id": "5",
+        "name": "Vladimir Keeling",
+        "email": "Louisa_Walsh18@hotmail.com"
+    },
+    {
+        "id": "6",
+        "name": "Gowtham",
+        "email": "gowtham404@yahoo.com"
+    },
+    {
+        "id": "7",
+        "name": "Vinoth",
+        "email": "vinoth45@hotmail.com"
+    },
+    {
+        "id": "8",
+        "name": "Alfredo Conn",
+        "email": "Clarabelle34@hotmail.com"
+    },
+    {
+        "id": "9",
+        "name": "Stan Shanahan",
+        "email": "Lamar.McClure@hotmail.com"
+    },
+    {
+        "id": "10",
+        "name": "Marvin Fay",
+        "email": "Osbaldo58@hotmail.com"
+    },
+    {
+        "id": "11",
+        "name": "Torrance Rau",
+        "email": "Orin45@gmail.com"
+    },
+    {
+        "id": "12",
+        "name": "Harold Gutmann MD",
+        "email": "Alyce.Stracke37@yahoo.com"
+    },
+    {
+        "id": "13",
+        "name": "Taryn Torphy",
+        "email": "Dean_Breitenberg71@hotmail.com"
+    },
+    {
+        "id": "14",
+        "name": "Bryana Lang",
+        "email": "Tatum.Ullrich@yahoo.com"
+    },
+    {
+        "id": "15",
+        "name": "Nyasia Green DDS",
+        "email": "Dino83@gmail.com"
+    },
+    {
+        "id": "16",
+        "name": "Nasir Gerhold",
+        "email": "Lilian_Bashirian8@hotmail.com"
+    },
+    {
+        "id": "17",
+        "name": "Raymundo Ritchie PhD",
+        "email": "Antwan.Schoen15@yahoo.com"
+    },
+    {
+        "id": "18",
+        "name": "Delmer Marvin",
+        "email": "Kiera62@yahoo.com"
+    },
+    {
+        "id": "19",
+        "name": "Rachel Wilkinson",
+        "email": "Foster_Conroy@gmail.com"
+    },
+    {
+        "id": "20",
+        "name": "Gladys Howell",
+        "email": "Constance.Labadie10@yahoo.com"
+    }];
+
+for (let i in arr) {
+    console.log(arr[i]);
+}
---
+++ DAY5/ResumeInJSONFormat.js
@@ -0,0 +1,10 @@
+let student = {
+    "name": "PARTHIBAN",
+    "fatherName": "Balasubramaniyan",
+    "motherName": "Selvarani",
+    "degree": "Mechanical Engineering",
+    "passedOut": 2019,
+    "HomeTown": "Theni",
+    "Hobby": "Read Books"
+}
+console.log(student);
