4466ddac029ba9866f646cf8f756555a465786af
parthibanb224 <parthibanb224@gmail.com>
2023-06-06 19:44:14 +0530

    DAY16 TASK

---
+++ DAY16/script.js
@@ -0,0 +1,78 @@
+let count1 = 11;
+setTimeout(() => {
+    count1--;
+    console.log(count1);
+    var target=document.getElementById('test');
+    target.innerText=count1;
+    setTimeout(() => {
+        count1--;
+        console.log(count1);
+        var target=document.getElementById('test');
+        target.innerText=count1;
+        setTimeout(() => {
+            count1--;
+            console.log(count1);
+            var target=document.getElementById('test');
+            target.innerText=count1;
+            setTimeout(() => {
+                count1--;
+                console.log(count1);
+                var target=document.getElementById('test');
+                target.innerText=count1;
+                setTimeout(() => {
+                    count1--;
+                    console.log(count1);
+                    var target=document.getElementById('test');
+                    target.innerText=count1;
+                    setTimeout(() => {
+                        count1--;
+                        console.log(count1);
+                        var target=document.getElementById('test');
+                        target.innerText=count1;
+                        setTimeout(() => {
+                            count1--;
+                            console.log(count1);
+                            var target=document.getElementById('test');
+                            target.innerText=count1;
+                            setTimeout(() => {
+                                count1--;
+                                console.log(count1);
+                                var target=document.getElementById('test');
+                                target.innerText=count1;
+                                setTimeout(() => {
+                                    count1--;
+                                    console.log(count1);
+                                    var target=document.getElementById('test');
+                                    target.innerText=count1;
+                                    setTimeout(() => {
+                                        count1--;
+                                        console.log(count1);
+                                        var target=document.getElementById('test');
+                                        target.innerText=count1;
+                                        setTimeout(() => {
+                                            count1--;
+                                            console.log(count1);
+                                            var target=document.getElementById('test');
+                                            target.innerText=count1;
+                                            clearTimeout();
+                                                console.log("SANTHOSH KUMAR ");
+                                                var target=document.getElementById('test');
+                                                target.innerText="HAPPY INDEPENDENCE DAY";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
+
+
+
+
+
+
+
